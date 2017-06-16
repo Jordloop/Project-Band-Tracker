03da3b7e5a3648022fe67eff1b56ed5d9e83686c
JordLoop <Jordloop@gmail.com>
2017-06-16 10:26:30 -0700

    add Update() to Venue.cs; all tests are passing

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -120,10 +120,10 @@ namespace BandTracker
       conn.Open();
 
       SqlCommand cmd = new SqlCommand("SELECT * FROM venues WHERE id = @VenueId;", conn );
+      
       SqlParameter VenueIdParameter = new SqlParameter();
       VenueIdParameter.ParameterName = "@VenueId";
       VenueIdParameter.Value = id.ToString();
-
       cmd.Parameters.Add(VenueIdParameter );
 
       SqlDataReader rdr = cmd.ExecuteReader();
@@ -148,6 +148,40 @@ namespace BandTracker
       }
       return foundVenue;
     }
+
+//Update()
+public void Update(string newName)
+{
+  SqlConnection conn = DB.Connection();
+  conn.Open();
+
+  SqlCommand cmd = new SqlCommand("UPDATE venues SET name = @NewName OUTPUT INSERTED.name WHERE id = @VenueId;", conn);
+
+  SqlParameter newNameParameter = new SqlParameter();
+  newNameParameter.ParameterName = "@NewName";
+  newNameParameter.Value = newName;
+  cmd.Parameters.Add(newNameParameter );
+
+  SqlParameter venueIdParameter = new SqlParameter();
+  venueIdParameter.ParameterName = "@VenueId";
+  venueIdParameter.Value = this.GetId();
+  cmd.Parameters.Add(venueIdParameter );
+
+  SqlDataReader rdr = cmd.ExecuteReader();
+
+  while(rdr.Read())
+  {
+    this._name = rdr.GetString(0 );
+  }
+  if(rdr != null )
+  {
+    rdr.Close();
+  }
+  if(conn != null )
+  {
+    conn.Close();
+  }
+}
 
 //DeleteAll()
     public static void DeleteAll()
