8827267b0eb876767e3485427f2ee6ef83675edd
JordLoop <Jordloop@gmail.com>
2017-06-16 09:39:01 -0700

    add GetAll_DatabaseIsEmpty_True() to VenueTest.cs; add GetAll() method to Band.cs and Venue.cs, all tests are passing

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -37,6 +37,35 @@ namespace BandTracker
 
 //Class Methods-----
 
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+
+      if (rdr != null )
+      {
+        rdr.Close();
+      }
+      if (conn != null )
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
 
 
   }
